3af3dc27360d3bfca04e2b6018596989510f7959
Kaiden Kliegl <132409071+kaidenkliegl@users.noreply.github.com>
2025-05-06 19:36:01 -0700

    finished frontend setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import './index.css';
-import configureStore from './store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 
 const store = configureStore();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+if (import.meta.env.MODE !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -15,10 +15,9 @@ if (import.meta.env.MODE === "production") {
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-// const configureStore = (preloadedState) => {
-//   return createStore(rootReducer, preloadedState, enhancer);
-// };
-const configureStore = createStore(rootReducer, enhancer);
-window.store = configureStore;
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
 
 export default configureStore;
